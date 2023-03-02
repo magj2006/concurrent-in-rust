19422d1695687203246464135ef35132ae270d1f
scarecrow <wilson@aptin.io>
2023-03-02 13:14:01 +0800

    using move

--- ch1-1-hello/src/main.rs
+++ ch1-1-hello/src/main.rs
@@ -2,18 +2,26 @@ use std::thread;
 
 fn main() {
     let t1 = thread::spawn(f);
-    let t2 = thread::spawn(f);
+
+    let numbers = vec![1, 3, 5, 7, 9];
+    let t2 = thread::spawn(move || {
+        let len = numbers.len();
+        let sum = numbers.iter().sum::<usize>();
+        sum / len
+    });
 
     println!("Hello from the main thread!");
 
     t1.join().unwrap();
-    t2.join().unwrap();
+    let average = t2.join().unwrap();
+
+    println!("average {average}")
 }
 
 fn f() {
-    println!("Hello from another thread!");
-
     let id = thread::current().id();
+
+    println!("Hello from another thread! {id:?}");
 
     println!("This is my thread id: {id:?}");
 }
